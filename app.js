b545b345e48ed54d0a435d369dfdaca109e741be
ASCll <hzxbpld@163.com>
2021-04-15 16:29:52 +0800

    chore: update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 // static/xxxx -> static/express-dev-prototype/xxx
 app.use(
   `/static/${packageJson.name}`,
-  express.static(join(__dirname, "./static"))
+  express.static(join(__dirname, "./dist/static"))
 );
 
 // 响应 route.json 供其他源使用
@@ -17,10 +17,9 @@ app.get("/route.json", (_request, response) => {
 });
 
 function indexHtml(_request, response) {
-  response.type("html").sendFile(join(__dirname, "./src/index.html"));
+  response.type("html").sendFile(join(__dirname, "./dist/index.html"));
 }
 
-app.get("/", indexHtml);
 app.get("/app2", indexHtml);
 app.get("/helloworld", indexHtml);
 
